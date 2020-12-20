fed9d75ac3706481eb265daeac4464955d61fe83
Akira Kano <a.kano1101@gmail.com>
2020-12-20 14:35:03 +0900

    ✨簡単なあいさつプログラムでテスト成功

---
+++ app/lib/main.rb
@@ -0,0 +1,20 @@
+
+class Main
+  def self.greeting
+    puts 'hello use-ruby-imgui'
+  end
+end
+
+if __FILE__ == $0
+  Main.greeting
+end
+
+# require './ruby-imgui/sample/basic_usage'
+
+# class Window
+#   extend ImGuiDemo::BasicWindow
+# end
+
+# if __FILE__ == $0
+#   Window.Show
+# end
